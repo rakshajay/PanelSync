36ac6a404e910dae207335ef9a38a02cf8da0b80
rakshajay <rakshashettyhs@gmail.com>
2025-09-24 15:12:04 -0700

    Resource folder added

--- Scripts/latest_obj.js
+++ Scripts/latest_obj.js
@@ -1,6 +1,6 @@
-//[09/22/2025]:Raksha- Auto-generated script
+// Auto-generated script
 
-var objPath = "C:/Users/raksh/OneDrive/Desktop/PanelSyncHot/Inventor/exports/obj/Expe1_P001_rA.obj";
+var objPath = "C:/Users/raksh/OneDrive/Desktop/PanelSyncHot/Inventor/exports/obj/CHECK2_P001_rA.obj";
 
 function log(m) { try { print(m); } catch (_) { } }
 
@@ -9,9 +9,14 @@ if (!objPath) { throw "objPath is missing!"; }
 log("Importing OBJ: " + objPath);
 var rc = SPoly.FromFile(objPath);
 if (!rc || rc.ErrorCode !== 0) { throw "SPoly.FromFile failed: " + JSON.stringify(rc); }
-var mesh = rc.PolyTbl[0];
-mesh.AddToDoc();
-log("Added mesh: " + mesh.GetName());
+if (!rc.PolyTbl || rc.PolyTbl.length === 0) { throw "No meshes found in OBJ"; }
+
+for (var i = 0; i < rc.PolyTbl.length; i++) {
+    var mesh = rc.PolyTbl[i];
+    mesh.AddToDoc();
+    log("Added mesh: " + mesh.GetName() + " (" + i + ")");
+}
+
 try { var vs = SViewSet.New(true); vs.Update(true); } catch(_) {}
 SaveDoc("", true);
 log("Saved project after import.");
